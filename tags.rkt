0953012287a1fe4e6648ef5f742754212c5d20df
Gregor.Kiczales <gregor.kiczales@gmail.com>
2018-04-24 11:05:46 -0700

    @Problem has a variety of checks on the problem number - starts at 1, adds by 1 each time, no skipped numbers etc.

--- tags.rkt
+++ tags.rkt
@@ -1,10 +1,14 @@
 #lang racket
 (require (for-syntax racket/base syntax/parse racket/list))
-(require syntax/keyword)
-(require htdp/testing (for-syntax htdp/testing))
 
 ;; Syntax for @tags including basic syntax checking.
-;; The tags are further checked through the grader framework.
+;;
+;; @tags are checked for well-formedness and referenced functions being defined
+;; during syntax expansion, they expand to just (values) so that they don't
+;; affect what values are printed when student code is run.
+;; 
+;; The file structure parser pulls apart the syntax later, knowing they
+;; have met minimal syntactic checking.
 
 (provide @Problem
 
@@ -16,13 +20,10 @@
          @template)
 
 
-;; @tags are checked for basic validity during parsing, they expand to
-;; just (values) so that they don't affect what values are printed when
-;; student code is run.
-;; The file structure parser pulls apart the syntax later, knowing they
-;; have met minimal syntactic checking.
 
-(define PRIMITIVE-TYPES '(Number Integer Natural String Boolean Image))
+
+(define PRIMITIVE-TYPES
+  '(Number Integer Natural String Boolean Image))
 
 (define-for-syntax TEMPLATE-ORIGINS
   '(;<TypeName>      decomposition and possible structural recursion
@@ -36,26 +37,27 @@
     accumulator
     for-each))
 
-
 (define-for-syntax DD-TEMPLATE-RULES
   '(atomic-non-distinct atomic-distinct one-of compound self-ref ref))
 
 
+(define-for-syntax PROBLEMS empty) ;(listof Natural), could be more later
+
+
+;; Sets up 2 phase expansion.  First phase is basic syntax, second phase checks
+;; that referenced functions are bound, problem numbers aren't duplicated etc.
 (define-syntax (define-@Tag-syntax stx)
   (syntax-case stx ()
     [(_ tag arity-string kind-string checker)
-     #`(define-syntax (tag stx)
-         
+     
+     #`(define-syntax (tag stx)         
          (unless (member (syntax-local-context) '(module top-level))            
-           (raise-syntax-error #f (format "Found ~a that is not at top level" 'tag) stx))
-         
+           (raise-syntax-error #f (format "Found ~a that is not at top level" 'tag) stx))         
          (syntax-case stx ()
            [(_)        (raise-syntax-error #f (format "expected ~a ~a after ~a" arity-string kind-string 'tag) stx)]
            [(_ . id) #'(#%expression (checker . id))]
            [_          (raise-syntax-error #f (format "expected an open parenthesis before ~a" 'tag) stx)]))]))
                                              
-
-
 
 (define-@Tag-syntax @Problem                    "one" "integer greater than 0"    check-problem-number)
 (define-@Tag-syntax @HtDF              "at least one" "function name"             check-defined-functions)
@@ -65,13 +67,26 @@
 (define-@Tag-syntax @dd-template-rules "at least one" "data driven template rule" check-dd-template-rules)
 
 
-(define-syntax (check-problem-number stx) ;!!! later make this check for duplicates  
+(define-syntax (check-problem-number stx)
   (syntax-case stx ()
     [(_ ns)
      (let ([n (syntax-e #'ns)])
        (unless (and (integer? n)
                     (> n 0))
-         (raise-syntax-error "Expected integer 1 or greater." stx #'ns))
+         (raise-syntax-error '@Problem "expected integer 1 or greater." stx #'ns))
+       (when (member n PROBLEMS)
+         (raise-syntax-error '@Problem (format "problem number ~a was used previously and cannot be re-used" n) stx #'ns))
+       (cond [(empty? PROBLEMS)
+              (unless (= n 1)
+                (raise-syntax-error '@Problem "expected first @Problem tag in file to be number 1" stx #'ns))]
+             [(not (= n (add1 (first PROBLEMS))))
+              (raise-syntax-error '@Problem
+                                  (format "previous @Problem tag is number ~a, expected this one to be number ~a"
+                                                    (first PROBLEMS)
+                                                    (add1 (first PROBLEMS)))
+                                  stx
+                                  #'ns)])
+       (set! PROBLEMS (cons n PROBLEMS))
        #'(values))]))
 
 (define-syntax (check-defined-functions stx)    
